c872573ef36052af5ed4584c31dc3cc8596275c5
Bluelobstae <feliramcos@gmail.com>
2025-07-16 18:53:55 -0400

    editar informe

--- backend/src/routes/informe.routes.js
+++ backend/src/routes/informe.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearInforme, dejarObservacion, resolverObservacion, aprobarInforme } from "../controllers/informe.controller.js";
+import { crearInforme, dejarObservacion, resolverObservacion, editarInforme, aprobarInforme } from "../controllers/informe.controller.js";
 import { authenticateJwt } from "../middleware/authentication.middleware.js";
 
 const router = Router();
@@ -8,6 +8,7 @@ router.use(authenticateJwt);
 router.post("/", crearInforme); // tesorero
 router.put("/:id/observacion", dejarObservacion); // presidente
 router.put("/:id/resolver", resolverObservacion); // tesorero
+router.put("/:id", editarInforme); // tesorero
 router.put("/:id/aprobar", aprobarInforme); // presidente
 
 export default router;
